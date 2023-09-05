8568528a401a906748127817c72909e9a29d65db
John Williams <cadfanw@gmail.com>
2023-09-05 09:45:36 +0100

    Exercise 3.1

--- Calculator.java
+++ Calculator.java
@@ -1,32 +1,39 @@
-import java.util.Random;
 import java.util.Scanner;
 class calculator {
     public static void main(String[] args) {
         System.out.println("Welcome to the Java calculator");
-        Random random = new Random();
+    
         Scanner scanner = new Scanner(System.in);
-        int randnum1 = random.nextInt(99);
-        int randnum2 = random.nextInt(99);
+        System.out.println("Enter the first number");
+        String inputnum1 = scanner.next();
+        System.out.println("Enter the second number");
+        String inputnum2 = scanner.next();
+
+        int num1 = Integer.parseInt(inputnum1);
+        int num2 = Integer.parseInt(inputnum2);
+        
+        
         int output = 0;
-        System.out.println("The numbers are " + randnum1 + " and " + randnum2 + "\n would you like to \n 1: + \n 2: - \n 3: *" );
+        System.out.println("The numbers are " + num1 + " and " + num2 + "\n would you like to \n 1: + \n 2: - \n 3: *" );
         String input = scanner.next();
         int inputparsed = Integer.parseInt(input); 
 
         if(inputparsed == 1){
-            output = randnum1 + randnum2;
-            System.out.println(randnum1 + " + " + randnum2 + " = " + output);
+            output = num1 + num2;
+            System.out.println(num1 + " + " + num2 + " = " + output);
         } else if (inputparsed == 2)
         {
-            output = randnum1 - randnum2;
-            System.out.println(randnum1 + " - " + randnum2 + " = " + output);
+            output = num1 - num2;
+            System.out.println(num1 + " - " + num2 + " = " + output);
         } else if (inputparsed == 3)
         {
-            output = randnum1 * randnum2;
-            System.out.println(randnum1 + " * " + randnum2 + " = " + output);
+            output = num1 * num2;
+            System.out.println(num1 + " * " + num2 + " = " + output);
         } else {
             System.out.println("Please re run this program and enter a number between 1 and 3");
         }
 
-        
+        scanner.close();
+
     }
 }
