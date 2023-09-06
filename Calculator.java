8d2200d0f1e79a830befd6b086aa021cfeaee82e
Cadfan Williams <WIL19851415@stu.gcs.ac.uk>
2023-09-06 09:38:55 +0100

    Exercise 4.1

--- Calculator.java
+++ Calculator.java
@@ -4,36 +4,86 @@ class calculator {
         System.out.println("Welcome to the Java calculator");
     
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the first number");
+        System.out.print("Enter the first number: ");
         String inputnum1 = scanner.next();
-        System.out.println("Enter the second number");
+        System.out.print("Enter the second number: ");
         String inputnum2 = scanner.next();
-
+        
         int num1 = Integer.parseInt(inputnum1);
         int num2 = Integer.parseInt(inputnum2);
         
-        
-        int output = 0;
         System.out.println("The numbers are " + num1 + " and " + num2 + "\n would you like to \n 1: + \n 2: - \n 3: *" );
         String input = scanner.next();
         int inputparsed = Integer.parseInt(input); 
-
+        scanner.close();
         if(inputparsed == 1){
-            output = num1 + num2;
-            System.out.println(num1 + " + " + num2 + " = " + output);
+            Add add = new Add(num1, num2);
+            System.out.println(add.getOutput());
+
         } else if (inputparsed == 2)
         {
-            output = num1 - num2;
-            System.out.println(num1 + " - " + num2 + " = " + output);
+            Subtract sub = new Subtract(num1, num2);
+            System.out.println(sub.getOutput());
         } else if (inputparsed == 3)
         {
-            output = num1 * num2;
-            System.out.println(num1 + " * " + num2 + " = " + output);
+           Multiply multiply = new Multiply(num1, num2);
+           System.out.println(multiply.getOutput());
         } else {
             System.out.println("Please re run this program and enter a number between 1 and 3");
         }
+    }
 
-        scanner.close();
 
+    static class Add {
+        public int num1;
+        public int num2;
+        public int output;
+        public String outputString;
+
+        public Add(int input1, int input2){
+            num1 = input1;
+            num2 = input2;
+            output = num1 + num2;
+            outputString = "The output is: ";
+        }
+        public String getOutput(){
+            outputString += Integer.toString(output);
+            return outputString;
+        }
+    }
+    static class Subtract{
+        public int num1;
+        public int num2;
+        public int output;
+        public String outputString;
+
+        public Subtract(int input1, int input2){
+            num1 = input1;
+            num2 = input2;
+            output = num1 - num2;
+            outputString = "The output is: ";
+        }
+        public String getOutput(){
+            outputString += Integer.toString(output);
+            return outputString;
+        }
+    }
+    static class Multiply{
+        public int num1;
+        public int num2;
+        public int output;
+        public String outputString;
+
+        public Multiply(int input1, int input2){
+            num1 = input1;
+            num2 = input2;
+            output = num1 * num2;
+            outputString = "The output is: ";
+        }
+        public String getOutput(){
+            outputString += Integer.toString(output);
+            return outputString;
+        }
     }
 }
+
