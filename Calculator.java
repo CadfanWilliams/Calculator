369bccce71d2983b5ebb3a03ec8f39ed63fa0b30
Cadfan Williams <WIL19851415@stu.gcs.ac.uk>
2023-09-07 10:42:31 +0100

    Exercise 5.1

--- Calculator.java
+++ Calculator.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
 class calculator {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Java calculator");
-    
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
-        String inputnum1 = scanner.next();
-        System.out.print("Enter the second number: ");
-        String inputnum2 = scanner.next();
+        System.out.println("Welcome to the Java calculator");
+        System.out.print("How many numbers would you like to operate on: ");
+        String numberAmount = scanner.next();
+        int numberAmountInt = Integer.parseInt(numberAmount);
+        int nums[] = new int[numberAmountInt];
         
-        int num1 = Integer.parseInt(inputnum1);
-        int num2 = Integer.parseInt(inputnum2);
         
-        System.out.println("The numbers are " + num1 + " and " + num2 + "\n would you like to \n 1: + \n 2: - \n 3: *" );
+        for(int i = 0; i < numberAmountInt; i++){
+            System.out.print("Enter number " + (i + 1) + ":");
+            nums[i] = Integer.parseInt(scanner.next());
+        }
+        
+        
+        
+        System.out.println("Would you like to \n 1: + \n 2: - \n 3: *" );
         String input = scanner.next();
         int inputparsed = Integer.parseInt(input); 
         scanner.close();
         if(inputparsed == 1){
-            Add add = new Add(num1, num2);
+            Add add = new Add(nums);
             System.out.println(add.getOutput());
 
         } else if (inputparsed == 2)
         {
-            Subtract sub = new Subtract(num1, num2);
+            Subtract sub = new Subtract(nums);
             System.out.println(sub.getOutput());
         } else if (inputparsed == 3)
         {
-           Multiply multiply = new Multiply(num1, num2);
+           Multiply multiply = new Multiply(nums);
            System.out.println(multiply.getOutput());
         } else {
             System.out.println("Please re run this program and enter a number between 1 and 3");
@@ -35,15 +39,14 @@ class calculator {
 
 
     static class Add {
-        public int num1;
-        public int num2;
         public int output;
         public String outputString;
 
-        public Add(int input1, int input2){
-            num1 = input1;
-            num2 = input2;
-            output = num1 + num2;
+        public Add(int[] input){
+            for(int i = 0; i < input.length; i++)
+            {
+                output += input[i];
+            }
             outputString = "The output is: ";
         }
         public String getOutput(){
@@ -52,15 +55,15 @@ class calculator {
         }
     }
     static class Subtract{
-        public int num1;
-        public int num2;
         public int output;
         public String outputString;
 
-        public Subtract(int input1, int input2){
-            num1 = input1;
-            num2 = input2;
-            output = num1 - num2;
+        public Subtract(int[] input){
+
+              for(int i = 0; i < input.length; i++)
+            {
+                output -= input[i];
+            }
             outputString = "The output is: ";
         }
         public String getOutput(){
@@ -69,15 +72,15 @@ class calculator {
         }
     }
     static class Multiply{
-        public int num1;
-        public int num2;
-        public int output;
+        public int output = 1;
         public String outputString;
 
-        public Multiply(int input1, int input2){
-            num1 = input1;
-            num2 = input2;
-            output = num1 * num2;
+        public Multiply(int[] input){
+
+            for(int i = 0; i < input.length; i++)
+            {
+                output *= input[i];
+            }
             outputString = "The output is: ";
         }
         public String getOutput(){
