fa70faa07d3b1b3dda249b19c86cb4668cfa7a0d
John Cadfan Williams <cadfanw@gmail.com>
2023-09-15 13:49:45 +0100

    Exercise 5.2

--- Calculator.java
+++ Calculator.java
@@ -1,24 +1,41 @@
 import java.util.Scanner;
-class calculator {
+import java.util.ArrayList;
+
+class calculator
+{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //This is for user input
         System.out.println("Welcome to the Java calculator");
-        System.out.print("How many numbers would you like to operate on: ");
-        String numberAmount = scanner.next();
-        int numberAmountInt = Integer.parseInt(numberAmount);
-        int nums[] = new int[numberAmountInt];
-        
-        
-        for(int i = 0; i < numberAmountInt; i++){
-            System.out.print("Enter number " + (i + 1) + ":");
-            nums[i] = Integer.parseInt(scanner.next());
+
+        boolean finishedCollectingNumbers = false; //a boolean to flag whether the user has finished entering numbers
+
+        ArrayList<String> input = new ArrayList<String>(); // arraylist for all of the user inputs
+        ArrayList<Integer> nums = new ArrayList<Integer>(); //arraylist for all of the user inputs which are valid numbers
+        int i = 0; //loop index
+        while(finishedCollectingNumbers == false)
+        {
+            System.out.println("Please enter a number or if you are finished, enter 'Done' ");
+            input.add(scanner.next().toLowerCase()); //using .toLowerCase as if they enter "Done" instead of "done" it doesnt work
+
+
+            if(input.get(i).equals("done"))
+            {
+                //if the user enters "done" then the user is finished entering numbers and will break the loop
+                finishedCollectingNumbers = true;
+            }
+            else //With this we just have to hope that if what they enter isnt "done" then it is an acceptable number...
+            {
+                nums.add(Integer.parseInt(input.get(i)));
+
+                //in future I can add validation techniques to ensure that the data entered is safe.
+            }
+
+            i++;
         }
-        
-        
-        
+
         System.out.println("Would you like to \n 1: + \n 2: - \n 3: *" );
-        String input = scanner.next();
-        int inputparsed = Integer.parseInt(input); 
+        String operatorInput = scanner.next();
+        int inputparsed = Integer.parseInt(operatorInput);
         scanner.close();
         if(inputparsed == 1){
             Add add = new Add(nums);
@@ -30,11 +47,13 @@ class calculator {
             System.out.println(sub.getOutput());
         } else if (inputparsed == 3)
         {
-           Multiply multiply = new Multiply(nums);
-           System.out.println(multiply.getOutput());
+            Multiply multiply = new Multiply(nums);
+            System.out.println(multiply.getOutput());
         } else {
             System.out.println("Please re run this program and enter a number between 1 and 3");
         }
+
+
     }
 
 
@@ -42,10 +61,10 @@ class calculator {
         public int output;
         public String outputString;
 
-        public Add(int[] input){
-            for(int i = 0; i < input.length; i++)
+        public Add(ArrayList<Integer> input){
+            for(int i = 0; i < input.size(); i++)
             {
-                output += input[i];
+                output += input.get(i);
             }
             outputString = "The output is: ";
         }
@@ -58,11 +77,11 @@ class calculator {
         public int output;
         public String outputString;
 
-        public Subtract(int[] input){
+        public Subtract(ArrayList<Integer> input){
 
-              for(int i = 0; i < input.length; i++)
+            for(int i = 0; i < input.size(); i++)
             {
-                output -= input[i];
+                output -= input.get(i);
             }
             outputString = "The output is: ";
         }
@@ -75,11 +94,11 @@ class calculator {
         public int output = 1;
         public String outputString;
 
-        public Multiply(int[] input){
+        public Multiply(ArrayList<Integer> input){
 
-            for(int i = 0; i < input.length; i++)
+            for(int i = 0; i < input.size(); i++)
             {
-                output *= input[i];
+                output *= input.get(i);
             }
             outputString = "The output is: ";
         }
